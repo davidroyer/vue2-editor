2fe307220c297528e521f225525669b0fa85961f
David Royer <droyer01@gmail.com>
2016-11-30 03:49:01 -0500

    Updated build. Removed a few items from default toolbar

--- src/VueEditor.vue
+++ src/VueEditor.vue
@@ -22,11 +22,11 @@ var defaultToolbar = [
   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
   ['blockquote', 'code-block'],
 
-  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  // [{ 'header': 1 }, { 'header': 2 }],               // custom button values
   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  // [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
   [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-  [{ 'direction': 'rtl' }],                         // text direction
+  // [{ 'direction': 'rtl' }],                         // text direction
 
   // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
