582bce82df430e151fa1674fa1728e1748b5f239
David Royer <droyer01@gmail.com>
2017-02-06 23:52:58 -0500

    Updates

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>This is App.js</h1>
+    <vue-editor
+      :editorContent="contentForEditor"
+      @saveContent="handleSaveContent">
+      <!-- <template slot="buttonText">
+        Custom Save Button
+      </template> -->
+
+    </vue-editor>
+
+    <span>{{output}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      contentForEditor: "Some content here",
+      output: 'Starts'
+    }
+  },
+
+  methods: {
+    handleSaveContent: function (contentsToBeSaved) {
+      this.output = 'HELLO'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('working');
+import Vue from 'vue'
+import App from './App.vue'
+import '../dist/vue2-editor.css'
+import VueEditor from '../dist/vue2-editor.common'
+console.log(VueEditor);
+Vue.use(VueEditor)
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
