e48711b955d52469a8db49e0ce6d1a5b7707657e
David Royer <droyer01@gmail.com>
2018-02-03 21:33:38 -0500

    Added fullToolbar json file

---
+++ src/helpers/fullToolbar.js
@@ -0,0 +1,28 @@
+var fullToolbar = [
+  [{ 'font': [] }],
+
+  [{ 'header': [false, 1, 2, 3, 4, 5, 6, ] }],
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],
+
+  ['bold', 'italic', 'underline', 'strike'],
+
+  [{'align': ''}, {'align': 'center'}, {'align': 'right'}, {'align': 'justify'}],
+
+  [{ 'header': 1 }, { 'header': 2 }],
+
+  ['blockquote', 'code-block'],
+
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+
+  [{ 'script': 'sub'}, { 'script': 'super' }],
+
+  [{ 'indent': '-1'}, { 'indent': '+1' }],
+
+  [{ 'color': [] }, { 'background': [] }],
+
+  ['link', 'image', 'video', 'formula'],
+
+  [{ 'direction': 'rtl' }],
+  ['clean'],
+]
