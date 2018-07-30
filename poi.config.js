73f938bd6e257d63f6f85bfa509498394f591ce4
David Royer <droyer01@gmail.com>
2018-07-30 05:24:04 -0400

    removed docs and cleaned up poi config

--- poi.config.js
+++ poi.config.js
@@ -1,14 +1,10 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
   extractCSS: false,
   entry: path.resolve(__dirname, "./dev/index.js"),
   vendor: false,
-  // transformModules: ["quill-image-drop-module"],
-
   filename: {
     js: "vue2-editor.js"
-    // css: 'vue2-editor.css'
   }
 };
