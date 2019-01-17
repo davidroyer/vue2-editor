da590efa1b45379a3936a9e4bbe189145fb6db22
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2019-01-17 14:00:50 -0200

    fix: plugin

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,4 +1,12 @@
-import Vue from 'vue'
-import Vue2Editor from 'vue2-editor'
+const { resolve } = require('path')
 
-Vue.use(Vue2Editor)
+module.exports = function nuxtVue2Editor(moduleOptions) {
+  this.addPlugin({
+    ssr: false,
+    src: resolve(__dirname, 'plugin.js'),
+    fileName: 'vue2-editor.js',
+    moduleOptions
+  })
+}
+
+module.exports.meta = require('../package.json')
--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,12 +1,5 @@
-const { resolve } = require('path')
 
-module.exports = function nuxtVue2Editor(moduleOptions) {
-  this.addPlugin({
-    ssr: false,
-    src: resolve(__dirname, 'plugin.js'),
-    fileName: 'vue2-editor.js',
-    moduleOptions
-  })
-}
+import Vue from 'vue'
+import Vue2Editor from 'vue2-editor'
 
-module.exports.meta = require('../package.json')
+Vue.use(Vue2Editor)
