4182ceac8f2e894cf912861fcfcb7ed5441f8f14
David Royer <droyer01@gmail.com>
2019-05-16 14:38:16 -0400

    chore: testing vue-cli build

--- demos/views/Basic.vue
+++ demos/views/Basic.vue
@@ -26,10 +26,12 @@
 </template>
 
 <script>
-// import { VueEditor, Quill } from "../../src/index.js";
+import { VueEditor, Quill } from "../../dist/Vue2Editor.umd.min.js";
+console.log("TCL: VueEditor", VueEditor);
+console.log("TCL: Quill", Quill);
 
 export default {
-  // components: { VueEditor },
+  components: { VueEditor },
 
   data: () => ({
     toolbarSettings: [
