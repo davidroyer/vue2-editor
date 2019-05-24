8cfb6fd65f65d57248878c4e655d6182550109ad
David Royer <droyer01@gmail.com>
2019-05-24 19:07:43 -0400

    fix: fixes libraryName

--- dist/vue2-editor.umd.js
+++ dist/vue2-editor.umd.js
@@ -1,14 +1,12 @@
-console.log('FROM umd.js');
-
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["vue2-editor"] = factory();
+		exports["Vue2Editor"] = factory();
 	else
-		root["vue2-editor"] = factory();
+		root["Vue2Editor"] = factory();
 })((typeof self !== 'undefined' ? self : this), function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -14306,7 +14304,7 @@ module.exports = __webpack_require__(63);
 
 "use strict";
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"70c967fa-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueEditor.vue?vue&type=template&id=464d8f32&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"1f72939f-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueEditor.vue?vue&type=template&id=464d8f32&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"quillWrapper"},[_vm._t("toolbar"),_c('div',{ref:"quillContainer",attrs:{"id":_vm.id}}),(_vm.useCustomImageHandler)?_c('input',{ref:"fileInput",staticStyle:{"display":"none"},attrs:{"id":"file-upload","type":"file","accept":"image/*"},on:{"change":function($event){return _vm.emitImageInfo($event)}}}):_vm._e()],2)}
 var staticRenderFns = []
 
@@ -18133,6 +18131,6 @@ module.exports = listCacheHas;
 
 /***/ })
 
-/******/ });
+/******/ })["default"];
 });
 //# sourceMappingURL=vue2-editor.umd.js.map
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   css: {
     extract: false
+  },
+  configureWebpack: {
+    // entry: "./src/lib.js",
+    output: {
+      libraryExport: "default",
+      filename: "vue2-editor",
+      // libraryTarget: "window",
+      library: "Vue2Editor"
+    }
+  },
+  chainWebpack: config => {
+    config.plugins.delete("html");
+    // config.plugins.delete('preload')
+    // config.plugins.delete('prefetch')
   }
 };
+
+// genHTML: false,
