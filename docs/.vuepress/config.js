a02efbf492a34b41b2112cdd74c0eb7e7c6b5226
David Royer <droyer01@gmail.com>
2019-06-11 22:09:49 -0400

    docs: clean up `config.js`

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const { version } = require("../../package.json");
-console.log("TCL: version", version);
 
 module.exports = {
   head: [
@@ -33,16 +32,6 @@ module.exports = {
     require("./plugin.js"),
     "live",
     "demo-block",
-    // [
-    //   "demo-code",
-    //   {
-    //     onlineBtns: {
-    //       codepen: false,
-    //       jsfiddle: false,
-    //       codesandbox: false
-    //     }
-    //   }
-    // ],
 
     [
       "@vuepress/google-analytics",
@@ -95,22 +84,3 @@ module.exports = {
     }
   }
 };
-
-// module.exports = {
-//   title: "Vue2Editor",
-//   description:
-//     "Your editor for rich text editing built with Vue.js and Quill.js",
-//   ga: "UA-56060335-4",
-//   themeConfig: {
-//     sidebar: "auto",
-//     editLinks: true,
-//     // sidebar: 'auto',
-//     docsDir: "docs",
-//     nav: [
-//       { text: "Guide", link: "/guide/" },
-//       { text: "Examples", link: "/examples/" },
-//       // { text: "FAQ", link: "/faq/" },
-//       { text: "Github", link: "https://github.com/davidroyer/vue2-editor" }
-//     ]
-//   }
-// };
