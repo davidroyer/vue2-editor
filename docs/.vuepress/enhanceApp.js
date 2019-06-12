60f18454c3deb3f6cfae85c6ef93813814d5197f
David Royer <droyer01@gmail.com>
2019-06-12 03:31:43 -0400

    docs: removes unused declarations

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,9 +1,4 @@
-export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData // site metadata
-}) => {
+export default ({ Vue }) => {
   if (typeof window !== "undefined") {
     Vue.use(window.Vue2Editor);
   }
