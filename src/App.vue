b6486027e32319460c76a4287fc9aca0ed4e8ea9
David Royer <droyer01@gmail.com>
2019-08-04 00:07:12 -0400

    chore: update to dev

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,5 @@
 <template>
   <div id="app">
-    <!-- <Editor v-model="content" @text-change="handleTextChange" /> -->
-    <!-- <vue-editor
-      v-model="content"
-      use-markdown-shortcuts
-      :editor-options="editorOptions"
-    >
-      <div id="my-toolbar" slot="toolbar">
-        <span class="ql-formats">
-          <select class="ql-header" defaultValue="0">
-            <option value="1">Heading</option>
-            <option value="2">Subheading</option>
-            <option value="0">Normal</option>
-          </select>
-          <select class="ql-font">
-            <option></option>
-            <option value="serif"></option>
-            <option value="monospace"></option>
-          </select>
-        </span>
-        <span class="ql-formats">
-          <button class="ql-bold"></button>
-          <button class="ql-italic"></button>
-          <button class="ql-underline"></button>
-        </span>
-        <span class="ql-formats">
-          <select class="ql-color"></select>
-          <select class="ql-background"></select>
-        </span>
-        <span class="ql-formats">
-          <button class="ql-list" value="ordered"></button>
-          <button class="ql-list" value="bullet"></button>
-          <select class="ql-align">
-            <option defaultValue></option>
-            <option value="center"></option>
-            <option value="right"></option>
-            <option value="justify"></option>
-          </select>
-        </span>
-        <span class="ql-formats">
-          <button class="ql-link"></button>
-          <button class="ql-image"></button>
-          <button class="ql-code-block"></button>
-        </span>
-        <span class="ql-formats">
-          <button class="ql-clean"></button>
-        </span>
-      </div>
-    </vue-editor>-->
     <VueEditor
       ref="vEditor"
       v-model="content"
@@ -58,39 +10,14 @@
 </template>
 
 <script>
+import Quill from "quill";
+const AlignStyle = Quill.import("attributors/style/align");
+Quill.register(AlignStyle, true);
+
 export default {
-  // components: { Editor },
-  data() {
-    return {
-      // eToolbar: [
-      //   [
-      //     { align: "" },
-      //     { align: "center" },
-      //     { align: "right" },
-      //     { align: "justify" }
-      //   ],
-      //   ["blockquote", "code-block"]
-      // ],
-      editorOptions: {
-        modules: {
-          // toolbar: "#my-toolbar"
-          // toolbar: [
-          //   [{ header: [false, 1, 2, 3, 4, 5, 6] }],
-          //   ["bold", "italic", "underline", "strike"], // toggled buttons
-          //   [
-          //     { align: "" },
-          //     { align: "center" },
-          //     { align: "right" },
-          //     { align: "justify" }
-          //   ]
-          //   // ["blockquote", "code-block"]
-          // ]
-        },
-        placeholder: "newww placeholder text here ..."
-      },
-      content: "<ul><li>Item One</li><li>Item Two</li></ul>"
-    };
-  },
+  data: () => ({
+    content: ""
+  }),
 
   methods: {
     handleTextChange(obj) {
