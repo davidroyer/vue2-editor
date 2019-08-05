d7b764874851212dbf5cc590af652a4b4c4e6a3f
David Royer <droyer01@gmail.com>
2019-08-04 23:05:57 -0400

    chore: dev update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
   <div id="app">
+    <button @click="deleteImage">Delete</button>
     <VueEditor
       ref="vEditor"
       v-model="content"
+      use-custom-image-handler
+      use-markdown-shortcuts
       @focus="onEditorFocus"
       @blur="onEditorBlur"
+      @imageAdded="handleImageAdded"
+      @imageRemoved="handleImageRemoved"
     />
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Quill from "quill";
 const AlignStyle = Quill.import("attributors/style/align");
 Quill.register(AlignStyle, true);
+
+const BlockEmbed = Quill.import("blots/block/embed");
+
+/**
+ * Customize image so we can add an `id` attribute
+ */
+class ImageBlot extends BlockEmbed {
+  static create(value) {
+    const node = super.create();
+    node.setAttribute("src", value.url);
+    node.setAttribute("id", value.id);
+    return node;
+  }
+
+  static value(node) {
+    return {
+      url: node.getAttribute("src"),
+      id: node.getAttribute("id")
+    };
+  }
+}
+
+ImageBlot.blotName = "image";
+ImageBlot.tagName = "img";
+Quill.register(ImageBlot);
 
 export default {
   data: () => ({
@@ -30,6 +61,46 @@ export default {
 
     onEditorFocus(quill) {
       console.log("editor focus!", quill);
+    },
+
+    deleteImage(id) {
+      const ACCESS_TOKEN = "1d3ee7fe34576a55a24ec551061b2365b6ebaf23";
+
+      axios({
+        url: `https://api.imgur.com/3/image/${id}`,
+        method: "DELETE",
+        headers: { Authorization: "Bearer " + ACCESS_TOKEN }
+      }).then(result => console.log("DELETE RESULT: ", result));
+    },
+
+    handleImageAdded(file, Editor, cursorLocation) {
+      const CLIENT_ID = "993793b1d8d3e2e";
+      var formData = new FormData();
+      formData.append("image", file);
+      axios({
+        url: "https://api.imgur.com/3/image",
+        method: "POST",
+        headers: { Authorization: "Client-ID " + CLIENT_ID },
+        data: formData
+      })
+        .then(result => {
+          const { link, id } = result.data.data;
+          Editor.insertEmbed(
+            cursorLocation,
+            "image",
+            {
+              id,
+              url: link
+            },
+            Quill.sources.USER
+          );
+        })
+        .catch(err => console.log(err));
+    },
+
+    handleImageRemoved(image) {
+      console.log("handleImageRemoved -> image", image);
+      this.deleteImage(image.id);
     }
   }
 };
