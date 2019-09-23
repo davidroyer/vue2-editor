c0ea474e8e3a31f1e7479ba2d030c147ca813c53
David Royer <droyer01@gmail.com>
2019-09-23 01:06:30 -0400

    chore: update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-console */
 import VueEditor from "./components/VueEditor.vue";
-import VSimple from "./components/VSimple.vue";
-import VComp from "./components/VComp.js";
+// import VSimple from "./components/VSimple.vue";
+// import VComp from "./components/VComp.js";
 import NEditor from "./components/NEditor.js";
-console.log("TCL: VComp", VComp);
+// console.log("TCL: VComp", VComp);
 // eslint-disable-next-line no-console
 console.log("TCL: VueEditor", VueEditor);
 
 const Plugin = {
   install(Vue, options = {}) {
     Vue.component("VueEditor", VueEditor);
-    Vue.component("VSimple", VSimple);
-    Vue.component("VComp", VComp);
+    // Vue.component("VSimple", VSimple);
+    // Vue.component("VComp", VComp);
     Vue.component("NEditor", NEditor);
   }
 };
